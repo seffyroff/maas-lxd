5afb398dacc356609fe4045d80bf976aed969cd2
Phil Merricks <seffyroff@gmail.com>
2018-10-04 17:55:52 -0700

    removed pxe redirect

--- make-maas.sh
+++ make-maas.sh
@@ -25,13 +25,13 @@ sleep 10
 # Setup LXD forward for pxe requests
 # TODO: Make this work on 2.0.x stable LXC
 IPADDRESS=$(lxc info $CONTAINER | awk -F"[: \t]+" '/eth0:.*inet[^6]/ {print $4}')
-# if [ $MAJOR -eq 2 ] && [ $MINOR -eq 0 ]
-# then
-    echo "LXC Stable branch not scripted for dnsmasq settings PXE will not work"
-    echo "To install the backport: apt install -t xenial-backports lxc lxc-client"
-# else
-    echo "Setting up pxe redirect for IP $IPADDRESS"
-#     lxc network set lxdbr0 raw.dnsmasq dhcp-boot=pxelinux.0,$CONTAINER,$IPADDRESS && systemctl restart lxd.service
-# fi
+# # if [ $MAJOR -eq 2 ] && [ $MINOR -eq 0 ]
+# # then
+#     echo "LXC Stable branch not scripted for dnsmasq settings PXE will not work"
+#     echo "To install the backport: apt install -t xenial-backports lxc lxc-client"
+# # else
+#     echo "Setting up pxe redirect for IP $IPADDRESS"
+# #     lxc network set lxdbr0 raw.dnsmasq dhcp-boot=pxelinux.0,$CONTAINER,$IPADDRESS && systemctl restart lxd.service
+# # fi
 
 echo "MAAS will become available at: http://$IPADDRESS:5240/MAAS with user/password admin/admin"
